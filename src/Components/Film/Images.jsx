781c1c433f64ed08c3a38941b372872df4b8b983
Tim Zoltie <t.zoltie@gmail.com>
2024-05-23 18:23:00 +0100

    css updated for images componet

--- src/Components/Film/Images.jsx
+++ src/Components/Film/Images.jsx
@@ -1,31 +1,70 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import back from "../../assets/svg/backarrow.svg";
 
 export default function Images() {
+  const [film, setFilm] = useState({});
+  const urlPararms = useParams();
 
-    const [images, setImages] = useState({})
-    const urlPararms = useParams();
-
-    useEffect(() => {
-        fetch(
-            `https://api.themoviedb.org/3/movie/${urlPararms.id}?api_key=017864b5160abacb16620d2413135901&append_to_response=videos,images`
-        )
-        .then(response => response.json())
-        .then(json => setImages(json))
-    }, [urlPararms, setImages])
-    console.log(images)
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${urlPararms.id}?api_key=017864b5160abacb16620d2413135901&append_to_response=videos,images`
+    )
+      .then((response) => response.json())
+      .then((json) => setFilm(json));
+  }, [urlPararms, setFilm]);
+  console.log(film);
 
   return (
-      <div className="images-container">
-        <section className="posters-box">
-          <h2>Posters</h2>
-        </section>
-        <section className="backdrops-box">
-          <h2>Backdrops</h2>
-        </section>
-        <section className="logos-box">
-          <h2>Logos</h2>
-        </section>
-      </div>
+    <div>
+      {film && (
+        <>
+        <Link to={`/${film.id}`}>
+            <img src={back} className="icon" id="back" />
+        </Link>
+          <div className="images-container">
+            <section className="posters-box">
+              <h2>Posters</h2>
+              <ul className="film-image-list">
+                {film.images.posters.map((image, index) => (
+                  <li key={index}>
+                    <img
+                      src={`http://image.tmdb.org/t/p/w92${image.file_path}`}
+                      alt="poster"
+                    />
+                  </li>
+                ))}
+              </ul>
+            </section>
+            <section className="backdrops-box">
+              <h2>Backdrops</h2>
+              <ul className="film-image-list">
+                {film.images.backdrops.map((image, index) => (
+                  <li key={index}>
+                    <img
+                      src={`http://image.tmdb.org/t/p/w92${image.file_path}`}
+                      alt="backdrop images"
+                    />
+                  </li>
+                ))}
+              </ul>
+            </section>
+            <section className="logos-box">
+              <h2>Logos</h2>
+              <ul className="film-image-list">
+                {film.images.logos.map((image, index) => (
+                  <li key={index}>
+                    <img
+                      src={`http://image.tmdb.org/t/p/w92${image.file_path}`}
+                      alt="backdrop images"
+                    />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
